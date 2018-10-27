1897d04d2258c33a9752959ffb895df424415594
kumarmudur <kumarmudur10@gmail.com>
2018-10-27 13:37:02 +0530

    mongodb setup

--- config/config.js
+++ config/config.js
@@ -1,5 +1,9 @@
 const port = process.env.PORT || 8080;
+const mongoURI = 'mongodb://kumar10:shiva10@ds125293.mlab.com:25293/devconnect';
+const secretKey = 'secret';
 
 module.exports = {
-    port
+    port,
+    mongoURI,
+    secretKey
 };
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const { port } = require('./config/config');
+
+//db config
+const db = require('./config/config').mongoURI;
+
+//connect to mongodb
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => {
+        console.log('mongodb connected');
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 app.get('/', (req, res) => {
     res.send('Hi nodejs app');
